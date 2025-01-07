6e64fead6bace07ab12625aa8b991b6be65215f7
VictorD91 <victordiazocampo@gmail.com>
2025-01-07 11:48:45 +0100

    Update discover.sh

--- 9. HERRAMIENTAS/2. ANALISIS DE REDES/discover.sh
+++ 9. HERRAMIENTAS/2. ANALISIS DE REDES/discover.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+#Creamos la funcion control_c para detener el proceso
 function control_c() {
         echo "Proceso detenido"
         exit 1
 }
-
+ 
 trap control_c SIGINT
 
+#Con este bucle hará ping a todo el rango de red para ver cual es el que está activo
 for i in {1..255}; do
         timeout 1 bash -c "ping -c 1 10.0.2.$i" >/dev/null
         if [ $? -eq 0 ]; then
