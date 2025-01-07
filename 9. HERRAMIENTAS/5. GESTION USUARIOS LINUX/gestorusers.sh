36dd3f9734e27818fbf7e6dc2604432b0e2250be
VictorD91 <victordiazocampo@gmail.com>
2025-01-07 11:44:54 +0100

    Update gestorusers.sh

--- 9. HERRAMIENTAS/5. GESTION USUARIOS LINUX/gestorusers.sh
+++ 9. HERRAMIENTAS/5. GESTION USUARIOS LINUX/gestorusers.sh
@@ -1,35 +1,36 @@
 #/bin/bash
 
+#Creamos dos funciones para crear o borrar el usuario que le pase un usuario
 create_user() {
-	read -p "Ingrese el nombre del usuario que desea crear: " username
-	sudo adduser $username
-	echo "Usuario $username creado"
+        read -p "Ingrese el nombre del usuario que desea crear: " username
+        sudo adduser $username
+        echo "Usuario $username creado"
 }
-
 delete_user() {
-	read -p "Ingrese el nombre del usuario que desea borrar: " username
-	sudo deluser --remove-home $username
-	echo "Usuario $username borrado"
+        read -p "Ingrese el nombre del usuario que desea borrar: " username
+        sudo deluser --remove-home $username
+        echo "Usuario $username borrado"
 }
 
+#Creamos un bucle que en el caso de que un usuario le pase un número llame a una función elegida
 while true; do
-	echo "1) Crear usuario"
-	echo "2) Borrar usuario"
-	echo "3) Salir"
-	read -p "Elija una de las 3 opciones: " election
+        echo "1) Crear usuario"
+        echo "2) Borrar usuario"
+        echo "3) Salir"  
+        read -p "Elija una de las 3 opciones: " election
 
-	case $election in
-		1)
-			create_user
-			;;
-		2)
-			delete_user
-			;;
-		3)
-			exit 1
-			;;
-		*)
-			echo "Opción no válida"
-			;;
-	esac
+        case $election in 
+                1) 
+                        create_user
+                        ;;
+                2) 
+                        delete_user
+                        ;;
+                3)
+                        exit 1
+                        ;;
+                *)
+                        echo "Opción no válida"
+                        ;;
+        esac
 done
